84168fd672e2af23cf478ea34f1d95546f2bea6f
Decrypt <louis.vicomte@protonmail.com>
2024-05-15 18:36:46 +0200

    Update custom-header.js

--- javascripts/discourse/initializers/custom-header.js
+++ javascripts/discourse/initializers/custom-header.js
@@ -1,46 +1,32 @@
 // javascripts/discourse/initializers/custom-header.js
 
-import { withPluginApi } from "discourse/lib/plugin-api";
+import { withPluginApi } from 'discourse/lib/plugin-api';
+import { h } from 'virtual-dom';
 
 export default {
-  name: "custom-header-buttons",
+  name: 'custom-header-buttons',
 
   initialize() {
-    withPluginApi("0.8", (api) => {
-      api.decorateWidget("header:before", (helper) => {
-        // Check screen width to add buttons only on desktop and tablet
-        if (window.innerWidth <= 768) {
-          return;
-        }
+    withPluginApi('0.8', api => {
+      api.createWidget('custom-header-buttons', {
+        tagName: 'div.custom-header-buttons',
+        buildKey: () => 'custom-header-buttons',
 
-        // Ensure this code only runs once
-        if (document.querySelector(".custom-header-buttons")) {
-          return;
-        }
-
-        const logoWrapper = document.querySelector(".home-logo-wrapper-outlet");
-        if (logoWrapper) {
-          const buttonsContainer = document.createElement("div");
-          buttonsContainer.className = "custom-header-buttons";
-
-          buttonsContainer.innerHTML = `
-            <a href="https://cr.cryptoast.fr/t/tuto-de-prise-en-main-de-la-plateforme/7" class="header-button">
-              <span class="text-version">Tuto prise en main</span>
-            </a>
-            <a href="https://cr.cryptoast.fr/t/lancement-du-parcours-de-formation/1444" class="header-button">
-              <span class="text-version">Formation</span>
-            </a>
-            <a href="https://cr.cryptoast.fr/tag/cr/?order=created&period=all" class="header-button">
-              <span class="text-version">Dernières analyses</span>
-            </a>
-          `;
-
-          logoWrapper.parentNode.insertBefore(
-            buttonsContainer,
-            logoWrapper.nextSibling
-          );
+        html() {
+          return [
+            h('a.header-button', { href: 'https://cr.cryptoast.fr/t/tuto-de-prise-en-main-de-la-plateforme/7' }, 
+              h('span.text-version', 'Tuto prise en main')),
+            h('a.header-button', { href: 'https://cr.cryptoast.fr/t/lancement-du-parcours-de-formation/1444' }, 
+              h('span.text-version', 'Formation')),
+            h('a.header-button', { href: 'https://cr.cryptoast.fr/tag/cr/?order=created&period=all' }, 
+              h('span.text-version', 'Dernières analyses'))
+          ];
         }
       });
+
+      api.decorateWidget('header-buttons:before', helper => {
+        return helper.attach('custom-header-buttons');
+      });
     });
-  },
+  }
 };
