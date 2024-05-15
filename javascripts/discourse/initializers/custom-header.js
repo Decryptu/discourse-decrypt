72bf7d6ae8c74f29acd1b491afc2286e104b2ec7
Decrypt <louis.vicomte@protonmail.com>
2024-05-15 18:33:10 +0200

    Update custom-header.js

--- javascripts/discourse/initializers/custom-header.js
+++ javascripts/discourse/initializers/custom-header.js
@@ -7,24 +7,39 @@ export default {
 
   initialize() {
     withPluginApi("0.8", (api) => {
-      api.decorateWidget("header-buttons:before", (helper) => {
-        // Create container for custom buttons
-        const buttonsContainer = document.createElement("div");
-        buttonsContainer.className = "custom-header-buttons";
+      api.decorateWidget("header:before", (helper) => {
+        // Check screen width to add buttons only on desktop and tablet
+        if (window.innerWidth <= 768) {
+          return;
+        }
 
-        buttonsContainer.innerHTML = `
-          <a href="https://cr.cryptoast.fr/t/tuto-de-prise-en-main-de-la-plateforme/7" class="header-button">
-            <span class="text-version">Tuto prise en main</span>
-          </a>
-          <a href="https://cr.cryptoast.fr/t/lancement-du-parcours-de-formation/1444" class="header-button">
-            <span class="text-version">Formation</span>
-          </a>
-          <a href="https://cr.cryptoast.fr/tag/cr/?order=created&period=all" class="header-button">
-            <span class="text-version">Dernières analyses</span>
-          </a>
-        `;
+        // Ensure this code only runs once
+        if (document.querySelector(".custom-header-buttons")) {
+          return;
+        }
 
-        return buttonsContainer;
+        const logoWrapper = document.querySelector(".home-logo-wrapper-outlet");
+        if (logoWrapper) {
+          const buttonsContainer = document.createElement("div");
+          buttonsContainer.className = "custom-header-buttons";
+
+          buttonsContainer.innerHTML = `
+            <a href="https://cr.cryptoast.fr/t/tuto-de-prise-en-main-de-la-plateforme/7" class="header-button">
+              <span class="text-version">Tuto prise en main</span>
+            </a>
+            <a href="https://cr.cryptoast.fr/t/lancement-du-parcours-de-formation/1444" class="header-button">
+              <span class="text-version">Formation</span>
+            </a>
+            <a href="https://cr.cryptoast.fr/tag/cr/?order=created&period=all" class="header-button">
+              <span class="text-version">Dernières analyses</span>
+            </a>
+          `;
+
+          logoWrapper.parentNode.insertBefore(
+            buttonsContainer,
+            logoWrapper.nextSibling
+          );
+        }
       });
     });
   },
