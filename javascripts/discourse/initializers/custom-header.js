3c62b3d6c47b956467dc746644d86e6b89923a6d
Decrypt <louis.vicomte@protonmail.com>
2024-05-15 20:41:36 +0200

    Update custom-header.js

--- javascripts/discourse/initializers/custom-header.js
+++ javascripts/discourse/initializers/custom-header.js
@@ -16,7 +16,7 @@ export default {
           return [
             h('a.header-button', { href: 'https://cr.cryptoast.fr/t/tuto-de-prise-en-main-de-la-plateforme/7' }, 
               h('span.text-version', 'Tuto prise en main')),
-            h('a.header-button', { href: 'https://cr.cryptoast.fr/t/lancement-du-parcours-de-formation/1444' }, 
+            h('a.header-button', { href: 'https://cr.cryptoast.fr/t/formation-fondamentale/1765' }, 
               h('span.text-version', 'Formation')),
             h('a.header-button', { href: 'https://cr.cryptoast.fr/tag/cr/?order=created&period=all' }, 
               h('span.text-version', 'Dernières analyses'))
