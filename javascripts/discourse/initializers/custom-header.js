c083a25a4eaa261293dc3f774ee141f9f4c602f0
Decrypt <louis.vicomte@protonmail.com>
2024-05-15 18:51:54 +0200

    mobile

--- javascripts/discourse/initializers/custom-header.js
+++ javascripts/discourse/initializers/custom-header.js
@@ -1,32 +1,92 @@
 // javascripts/discourse/initializers/custom-header.js
 
-import { withPluginApi } from 'discourse/lib/plugin-api';
-import { h } from 'virtual-dom';
+import { withPluginApi } from "discourse/lib/plugin-api";
+import { h } from "virtual-dom";
 
 export default {
-  name: 'custom-header-buttons',
+  name: "custom-header-buttons",
 
   initialize() {
-    withPluginApi('0.8', api => {
-      api.createWidget('custom-header-buttons', {
-        tagName: 'div.custom-header-buttons',
-        buildKey: () => 'custom-header-buttons',
+    withPluginApi("0.8", (api) => {
+      api.createWidget("custom-header-buttons", {
+        tagName: "div.custom-header-buttons",
+        buildKey: () => "custom-header-buttons",
 
         html() {
-          return [
-            h('a.header-button', { href: 'https://cr.cryptoast.fr/t/tuto-de-prise-en-main-de-la-plateforme/7' }, 
-              h('span.text-version', 'Tuto prise en main')),
-            h('a.header-button', { href: 'https://cr.cryptoast.fr/t/lancement-du-parcours-de-formation/1444' }, 
-              h('span.text-version', 'Formation')),
-            h('a.header-button', { href: 'https://cr.cryptoast.fr/tag/cr/?order=created&period=all' }, 
-              h('span.text-version', 'Dernières analyses'))
-          ];
-        }
+          const isMobile = window.innerWidth <= 768;
+
+          if (isMobile) {
+            return [
+              h(
+                "a.header-button",
+                {
+                  href: "https://cr.cryptoast.fr/t/tuto-de-prise-en-main-de-la-plateforme/7",
+                },
+                h(
+                  "svg",
+                  { viewBox: "0 0 24 24", width: "24", height: "24" },
+                  h("circle", { cx: "12", cy: "12", r: "10", fill: "black" })
+                )
+              ),
+              h(
+                "a.header-button",
+                {
+                  href: "https://cr.cryptoast.fr/t/lancement-du-parcours-de-formation/1444",
+                },
+                h(
+                  "svg",
+                  { viewBox: "0 0 24 24", width: "24", height: "24" },
+                  h("rect", {
+                    x: "4",
+                    y: "4",
+                    width: "16",
+                    height: "16",
+                    fill: "black",
+                  })
+                )
+              ),
+              h(
+                "a.header-button",
+                {
+                  href: "https://cr.cryptoast.fr/tag/cr/?order=created&period=all",
+                },
+                h(
+                  "svg",
+                  { viewBox: "0 0 24 24", width: "24", height: "24" },
+                  h("polygon", { points: "12,2 22,22 2,22", fill: "black" })
+                )
+              ),
+            ];
+          }
+            return [
+              h(
+                "a.header-button",
+                {
+                  href: "https://cr.cryptoast.fr/t/tuto-de-prise-en-main-de-la-plateforme/7",
+                },
+                h("span.text-version", "Tuto prise en main")
+              ),
+              h(
+                "a.header-button",
+                {
+                  href: "https://cr.cryptoast.fr/t/lancement-du-parcours-de-formation/1444",
+                },
+                h("span.text-version", "Formation")
+              ),
+              h(
+                "a.header-button",
+                {
+                  href: "https://cr.cryptoast.fr/tag/cr/?order=created&period=all",
+                },
+                h("span.text-version", "Dernières analyses")
+              ),
+            ];
+        },
       });
 
-      api.decorateWidget('header-buttons:before', helper => {
-        return helper.attach('custom-header-buttons');
+      api.decorateWidget("header-buttons:before", (helper) => {
+        return helper.attach("custom-header-buttons");
       });
     });
-  }
+  },
 };
